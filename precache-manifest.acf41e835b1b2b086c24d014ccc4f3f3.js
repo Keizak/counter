a9d929e5ffb34ca51b44d6e3de10b7c9b724f18a
Keizak <keizak@yandex.ru>
2020-05-22 22:12:38 +0300

    Updates

---
+++ precache-manifest.acf41e835b1b2b086c24d014ccc4f3f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0017fa91a16af655fbe87918e034340f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b463fa38b5ba749910",
+    "url": "/static/css/main.7aa3201b.chunk.css"
+  },
+  {
+    "revision": "6243f1f3d4493170c9dd",
+    "url": "/static/js/2.36db55a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36db55a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b463fa38b5ba749910",
+    "url": "/static/js/main.6baa5145.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
