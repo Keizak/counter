4387a8c1de2b15053bbda70f30f4114c5edac781
Keizak <keizak@yandex.ru>
2020-06-06 23:39:47 +0300

    New code.Part 2 .Rebuild component Button

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,20 @@ import Footer from "./Components/Footer/Footer";
 import Settings from "./Components/Settings/Settings";
 
 function App() {
-
+    let buttons_Start_Reset =[
+        {id: "1", height: "100px", width:"100px", backgroundColor:"green",
+            content:"start",borderRadius:"100%",onClick:"props.AddValueCounter"},
+        {id: "2", height: "100px", width:"100px", backgroundColor:"red",
+            content:"reset",borderRadius:"100%",onClick:{ResetCounter}}
+            ]
+    let max = 5
     let [value,setValue] = useState<number>(0)
     function AddValueCounter() {
         setValue(value += 1)
+        if (value === max)
+        {
+
+        }
     }
     function ResetCounter() {
         setValue(0)
@@ -21,11 +31,11 @@ function App() {
     }
     return (
         <div className={style.App}>
-            <Header value_counter={value}/>
-            <Settings
-                AddValueCounter={AddValueCounter}
-                ResetCounter={ResetCounter}
-                ChangeStartValue={ChangeStartValue}/>
+            <Header value_counter={value}
+                    AddValueCounter={AddValueCounter}
+                    ResetCounter={ResetCounter}
+                    buttons_Start_Reset={buttons_Start_Reset}/>
+            <Settings ChangeStartValue={ChangeStartValue}/>
             <Footer/>
 
         </div>)
