7c37b506a8169d718a4d9371d5734802e42b7055
Keizak <keizak@yandex.ru>
2020-06-09 15:06:21 +0300

    New code.Part 4 . Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,27 @@ import {rerenderEntireTree} from "./render";
 type AppType =
     {
         state: any
-        UpdateMaxValue: (maxvalue:string) => void
+        UpdateMaxValue: (maxvalue: string) => void
+        UpdateStartValue: (startvalue: string) => void
+        UpdateMaxValueVectors:(max:number) => void
+        UpdateStartValueVectors:(start:number) => void
     }
 
 function App(props: AppType) {
-    let [start, setStart] = useState<string>("0")
-    let [max, setMax] = useState<string>("5")
+    let a = String(props.state.Header.MaxValue)
+    let b = String(props.state.Header.StartValue)
+    let [start, setStart] = useState<string>(b)
+    let [max, setMax] = useState<string>(a)
     let [value, setValue] = useState<string>("0")
     let [color, setColor] = useState<string>("white")
     let [error, setError] = useState<boolean>(false)
 
     function AddValueCounter() {
-        let chislo = String(Number(value) + 1)
         let disabled = {
             id: "1", height: "100px", width: "100px", backgroundColor: "green",
             content: "start", borderRadius: "100%", disabled: true
         }
+        let chislo = String(Number(value )+ 1)
         if (Number(chislo) === Number(max)) {
             setError(true)
             props.state.Header.buttons_Start_Reset.shift()
@@ -33,6 +38,7 @@ function App(props: AppType) {
             rerenderEntireTree(props.state)
         }
         setValue(chislo)
+        rerenderEntireTree(props.state)
     }
 
     function ResetCounter() {
@@ -47,31 +53,35 @@ function App(props: AppType) {
         rerenderEntireTree(props.state)
     }
 
-    function ChangeMaxValue(maxvalue: string) {
-        if (Number(maxvalue) <= Number(value)) {
+    function ErrorValues(inputerror:boolean) {
+        if (inputerror) {
             setValue("Ошибка ввода")
-        } else setMax(maxvalue)
-
+            rerenderEntireTree(props.state)
+        }
+        else {
+            setValue(props.state.Header.StartValue)
+            rerenderEntireTree(props.state)
+        }
 
     }
 
+    function ChangeMaxValue(maxvalue: string) {
+        setValue(props.state.Header.StartValue)
+        props.state.Header.MaxValue=setMax(maxvalue)
+        rerenderEntireTree(props.state)
+    }
+
     function ChangeStartValue(startvalue: string) {
-        if (Number(startvalue) <= -1) {
-            setValue("Ошибка ввода")
-        } else
-            setStart(startvalue)
+        setValue(props.state.Header.StartValue)
+        props.state.Header.StartValue=setStart(startvalue)
+        rerenderEntireTree(props.state)
+
     }
 
     function ChangeColorValue(selectedcolor: string) {
         setColor(selectedcolor)
     }
-    function ChangeInput(maxvalue:string) {
-        if(Number(maxvalue) <= Number(value))
-        {
-            setValue("Ошибка ввода")
-        }
-        else setMax(maxvalue)
-    }
+
     return (
         <div className={style.App}>
             <Header error={error}
@@ -86,9 +96,11 @@ function App(props: AppType) {
             <Settings ChangeStartValue={ChangeStartValue}
                       ChangeColorValue={ChangeColorValue}
                       ChangeMaxValue={ChangeMaxValue}
+                      ErrorValues={ErrorValues}
                       UpdateMaxValue={props.UpdateMaxValue}
-                      newMaxValue={props.state.Settings.newMaxValue}
-                      newStartValue={props.state.Settings.newStartValue}
+                      UpdateStartValue={props.UpdateStartValue}
+                      newMaxValue={props.state.Settings.MaxValue}
+                      newStartValue={props.state.Settings.StartValue}
                       buttons_color={props.state.Settings.buttons_color}
             />
             <Footer/>
